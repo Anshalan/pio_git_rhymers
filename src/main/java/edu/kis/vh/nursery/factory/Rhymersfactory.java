75b87bb40f0fe79aac8ce15fa35f6f8be0fa1940
Maciej Czajkowski <maciejpiotrczajkowski@gmail.com>
2021-01-30 19:37:05 +0100

    1.4.1 stale i annotacje - zamiana wartosci na stale

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -4,15 +4,7 @@ import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
 public interface Rhymersfactory {
 
-<<<<<<< HEAD
-    public DefaultCountingOutRhymer GetStandardRhymer();
 
-    public DefaultCountingOutRhymer GetFalseRhymer();
-
-    public DefaultCountingOutRhymer GetFIFORhymer();
-
-    public DefaultCountingOutRhymer GetHanoiRhymer();
-=======
     public DefaultCountingOutRhymer getStandardRhymer();
 
     public DefaultCountingOutRhymer getFalseRhymer();
@@ -20,6 +12,5 @@ public interface Rhymersfactory {
     public DefaultCountingOutRhymer getFIFORhymer();
 
     public DefaultCountingOutRhymer getHanoiRhymer();
->>>>>>> 1.3.5.2 Fix method names
 
 }
